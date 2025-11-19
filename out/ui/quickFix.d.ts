a9bc23fdd32b0dfc8fe4025f999e89563248ca62
inboxBodyguard <ezmcyber@zohomail.com>
2025-11-19 05:53:40 +0100

    remove tracked build folder from git

--- out/ui/quickFix.d.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-import { AISecurityEngine } from '../ai/aiEngine';
-export declare class SecurityQuickFixProvider implements vscode.CodeActionProvider {
-    private aiEngine;
-    constructor(aiEngine: AISecurityEngine);
-    provideCodeActions(): vscode.CodeAction[];
-}
-//# sourceMappingURL=quickFix.d.ts.map
